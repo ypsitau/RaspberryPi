faca80baabaad9089eba2e8fb6c8e7913954ce70
ypsitau <ypsitau@nifty.com>
2024-06-05 16:48:11 +0900

    added test-pigpio

---
+++ test-pigpio/blink-led.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import pigpio
+import time
+
+pi = pigpio.pi()
+
+while True:
+    pi.set_PWM_dutycycle(26, 30)
+    time.sleep(1)
+    pi.set_PWM_dutycycle(26, 0)
+    time.sleep(1)
+
+pi.stop()
